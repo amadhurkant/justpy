87c4056431b7a47b2747a268eabe30698c362eca
amadhurkant <85044482+amadhurkant@users.noreply.github.com>
2023-01-07 22:10:36 +0530

    Update kfuncs.py

--- kfuncs.py
+++ kfuncs.py
@@ -3,7 +3,7 @@ def removeData(remove, data):
         raise Exception(f"RemoveNotError: Data to be removed- {remove} -not found in provided data")
     occurence = data.count(remove)
     if remove in data:
-        data0 =  data.split("0x", occurence)
+        data0 =  data.split(remove, occurence)
     data2 = ''
     for i in data0:
         data2 = data2+i
