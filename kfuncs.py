b9fb4fa67038f8fc968369c8805c16ec8b21de1d
amadhurkant <85044482+amadhurkant@users.noreply.github.com>
2023-01-07 22:09:15 +0530

    updated kfuncs
    Added checkHx

--- kfuncs.py
+++ kfuncs.py
@@ -3,8 +3,15 @@ def removeData(remove, data):
         raise Exception(f"RemoveNotError: Data to be removed- {remove} -not found in provided data")
     occurence = data.count(remove)
     if remove in data:
-        data0 =  data.split(remove, occurence)
+        data0 =  data.split("0x", occurence)
     data2 = ''
     for i in data0:
         data2 = data2+i
     return data2
+
+def checkHx(hexdat):
+    if len(hexdat) % 2 == 0:
+        return hexdat
+    else:
+        hexdat = "0"+hexdat
+    return hexdat
