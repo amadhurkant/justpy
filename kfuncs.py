d42361ad96a85a3b50fc7936dcf4037a0362a5da
amadhurkant <85044482+amadhurkant@users.noreply.github.com>
2023-07-16 22:30:44 +0530

    Update kfuncs.py
    See docs related to def ```eugclidianGCDTable``` function

--- kfuncs.py
+++ kfuncs.py
@@ -25,3 +25,24 @@ def checkHx(hexdat, h=0):
         else:
             hexdat = "0"+hexdat
             return hexdat
+
+def eugclidianGCDTable(a, b):
+    if b > a:
+        x = b
+        p = a
+        q, r = b // a, b % a
+    else:
+        x = a
+        p = b
+        q, r = a // b, a % b
+    n = 0
+    lister = [x, p, q, r]
+    dicti = {
+    n: lister
+    }
+    while True and lister[3]  != 0:
+        lister = [lister[1], lister[3], lister[1]//lister[3], lister[1]%lister[3]]
+        q, r, ra = p // r, r, p % r
+        n += 1
+        dicti[n] = lister
+    return dicti
